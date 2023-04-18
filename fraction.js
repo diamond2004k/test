19c1b3de5f3989194b96dd70abb099bb9ee82eae
diamond2004k <112924755+diamond2004k@users.noreply.github.com>
2023-04-18 10:47:39 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,60 @@
 //class unit fraction
+//Addition ,Subraction,Multiplication, Division.
+function calculate() {
+  const num1 = parseInt(prompt("Enter numerator of first fraction:"));
+  const den1 = parseInt(prompt("Enter denominator of first fraction:"));
+  const num2 = parseInt(prompt("Enter numerator of second fraction:"));
+  const den2 = parseInt(prompt("Enter denominator of second fraction:"));
+
+  const addResult = addFractions(num1, den1, num2, den2);
+  const subResult = subtractFractions(num1, den1, num2, den2);
+  const mulResult = multiplyFractions(num1, den1, num2, den2);
+  const divResult = divideFractions(num1, den1, num2, den2);
+
+  console.log(`Addition: ${addResult}`);
+  console.log(`Subtraction: ${subResult}`);
+  console.log(`Multiplication: ${mulResult}`);
+  console.log(`Division: ${divResult}`);
+}
+
+function addFractions(num1, den1, num2, den2) {
+  const resultNum = num1 * den2 + num2 * den1;
+  const resultDen = den1 * den2;
+  return simplifyFraction(resultNum, resultDen);
+}
+
+function subtractFractions(num1, den1, num2, den2) {
+  const resultNum = num1 * den2 - num2 * den1;
+  const resultDen = den1 * den2;
+  return simplifyFraction(resultNum, resultDen);
+}
+
+function multiplyFractions(num1, den1, num2, den2) {
+  const resultNum = num1 * num2;
+  const resultDen = den1 * den2;
+  return simplifyFraction(resultNum, resultDen);
+}
+
+function divideFractions(num1, den1, num2, den2) {
+  const resultNum = num1 * den2;
+  const resultDen = den1 * num2;
+  return simplifyFraction(resultNum, resultDen);
+}
+
+function simplifyFraction(num, den) {
+  const gcd = findGCD(num, den);
+  const simplifiedNum = num / gcd;
+  const simplifiedDen = den / gcd;
+  return `${simplifiedNum}/${simplifiedDen}`;
+}
+
+function findGCD(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return findGCD(b, a % b);
+  }
+}
+
+
+calculate();
